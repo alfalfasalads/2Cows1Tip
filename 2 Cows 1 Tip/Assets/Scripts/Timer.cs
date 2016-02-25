c62e2cde5bafb473512df017d2a85a51bba7182d
AbsoluteZero <dragonade30@gmail.com>
2016-02-24 20:58:42 -0500

    Implemented a Timer
    - Timer shows in the format "0:00"
    - Timer starts at 120 seconds
    - Created a prefab called "TimerCanvas"

---
+++ 2 Cows 1 Tip/Assets/Scripts/Timer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text m_TimerText;
+
+    float m_Timer = 0;
+    float m_RoundLength = 120.0f;
+
+    bool m_Paused = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_Timer = m_RoundLength;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_Timer <= 0)
+        {
+            return;
+        }
+
+        if (!m_Paused)
+        {
+            m_Timer -= Time.deltaTime;
+        }
+    }
+
+    public void OnGUI()
+    {
+        int minutes = Mathf.FloorToInt(m_Timer / 60f);
+        int seconds = Mathf.FloorToInt(m_Timer % 60);
+        m_TimerText.text = minutes.ToString("0") + ":" + seconds.ToString("00");
+    }
+}
